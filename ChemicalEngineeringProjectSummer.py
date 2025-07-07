397a5f0e922b20c7f2b9c0f83f4c91f782e6e7ce
Allan Rios <73731571+AllanGRios@users.noreply.github.com>
2025-07-07 19:41:33 +0200

    Add files via upload

---
+++ ChemicalEngineeringProjectSummer.py
@@ -0,0 +1,6 @@
+#Engineering Project Outline - Chemical Reactor Simulator
+def salycilic_acid(mol=1,temperature=25):
+    Melting_point = 158 #℃
+    Molecular_weight = 0.13812 #kg/mol
+    mass = (138.12*mol) #kg
+
